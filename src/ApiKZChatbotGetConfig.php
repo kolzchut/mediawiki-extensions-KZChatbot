4f8cc79fe00338e456a6ece47d6fc9f353dc2107
Joel Rothschild <joel@sofi.coop>
2024-08-04 12:29:59 +0300

    Added API endpoint for kzchatbot_get_config

---
+++ src/ApiKZChatbotGetConfig.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace MediaWiki\Extension\KZChatbot;
+
+use ApiBase;
+
+class ApiKZChatbotGetConfig extends ApiBase {
+	/**
+	 * Define form parameters and default values
+	 * @return array
+	 */
+	protected function getAllowedParams() {
+		return [
+			'uuid' => '',
+		];
+	}
+
+  /**
+   * Fetch configuration settings needed by the chatbot app.
+   */
+  public function execute() {
+		$result = $this->getResult();
+		$fieldNames = array_flip( KZChatbot::mappingDbToJson() );
+
+		// Look up UUID if given.
+		$uuid = $this->getParameter( 'uuid' );
+		if ( !empty( $uuid ) ) {
+			$userData = KZChatbot::getUserData( $uuid );
+			$uuid = $userData[ $fieldNames['uuid'] ];
+		}
+
+		// Assign new UUID if none is in current effect for the browser.
+		if ( empty( $userData ) ) {
+			$userData = KZChatbot::newUser();
+		}
+
+		// Provide updated cookie expiry.
+		$settings = KZChatbot::getGeneralSettings();
+		$cookieExpiryDays = $settings['cookie_expiry_days'] ?? 365;
+		$cookieExpiry = date( 'D, d M Y H:i:s \G\M\T', time() + $cookieExpiryDays * 60 * 60 * 24 );
+
+		// Prepare output
+		$config = [
+			'uuid' => $uuid,
+			'chatbotIsShown' => $userData[ $fieldNames['chatbotIsShown'] ],
+			'chatbotProminence' => $settings[ $fieldNames['chatbotProminence'] ],
+			'cookieExpiry' => $cookieExpiry,
+		];
+		$result->addValue( null, 'config', $config );
+  }
+
+}
--- src/KZChatbot.php
+++ src/KZChatbot.php
@@ -25,20 +25,99 @@ class KZChatbot {
 	}
 
 	/**
+	 * @return array
+	 */
+	public static function mappingDbToJson() {
+		return [
+			'kzcbu_uuid' => 'uuid',
+			'kzcbu_ip_address' => 'ip',
+			'kzcbu_is_shown' => 'chatbotIsShown',
+			'kzcbu_cookie_expiry' => 'cookieExpiry',
+			'kzcbu_last_active' => 'lastActive',
+			'kzcbu_questions_last_active_day' => 'questionsLastActiveDay',
+			'kzcbu_ranking_eligible_answer_id' => 'eligibleAnswerId',
+			'chatbot_prominence' => 'chatbotProminence'
+		];
+	}
+
+	/**
+	 * @return array|bool
+	 */
+	public static function newUser() {
+		$settings = self::getGeneralSettings();
+		$cookieExpiry = time() + ( $settings['cookie_expiry_days'] ?? 356 ) * 24 * 60 * 60;
+		$newUsersChatbotRate = $settings['new_users_chatbot_rate'] ?? 100;
+		$activeUsersLimit = $settings['active_users_limit'];
+		$activeUsersLimitDays = $settings['active_users_limit_days'] ?? 30;
+		// Show this user the chatbot?
+		$dbw = wfGetDB( DB_PRIMARY );
+		$currentAverage = $dbw->select(
+			[ 'kzchatbot_users' ],
+			[ 'AVG(kzcbu_is_shown) AS average' ],
+			[],
+			__METHOD__,
+		)->fetchRow();
+		$isShown = empty( $currentAverage['average'] ) ? ( $newUsersChatbotRate > 0 )
+			: ( $currentAverage['average'] <= ( $newUsersChatbotRate / 100 ) );
+		if ( $isShown && !empty( $activeUsersLimit ) ) {
+			// Need also to check that we haven't hit the absolute maximum on active users.
+			$activeUsersCount = $dbw->select(
+				[ 'kzchatbot_users' ],
+				[ 'COUNT(*) as count' ],
+				[ 'kzcbu_last_active <= ' . wfTimestamp( TS_MW, time() - ( $activeUsersLimitDays * 24 * 60 * 60 ) ) ],
+				__METHOD__,
+			)->fetchRow();
+			$isShown = empty( $activeUsersCount['count'] ) ? 1 : ( $activeUsersLimit < $activeUsersCount['count'] );
+		}
+		// Build and insert new user record
+		$userData = [
+			'kzcbu_uuid' => uniqid(),
+			'kzcbu_is_shown' => $isShown,
+			'kzcbu_cookie_expiry' => wfTimestamp( TS_MW, $cookieExpiry ),
+			'kzcbu_ip_address' => $_SERVER['SERVER_ADDR'],
+			'kzcbu_last_active' => wfTimestamp( TS_MW ),
+			'kzcbu_questions_last_active_day' => 0,
+		];
+		$dbw->insert( 'kzchatbot_users', $userData, __METHOD__ );
+		return $userData;
+	}
+
+	/**
+	 * @param string $uuid
+	 * @return array|bool
+	 */
+	public static function getUserData( $uuid ) {
+		$dbr = wfGetDB( DB_REPLICA );
+		$res = $dbr->select(
+			[ 'kzchatbot_users' ],
+			[
+				'kzcbu_uuid', 'kzcbu_ip_address', 'kzcbu_is_shown', 'kzcbu_cookie_expiry', 'kzcbu_last_active',
+				'kzcbu_questions_last_active_day', 'kzcbu_ranking_eligible_answer_id'
+			],
+			[ 'kzcbu_uuid' => $uuid ],
+			__METHOD__,
+		);
+		return $res->fetchRow();
+	}
+
+	/**
 	 * @return array|bool
 	 */
 	public static function getGeneralSettings() {
-		$dbr = wfGetDB( DB_REPLICA );
-		$generalSettingsNames = self::getGeneralSettingsNames();
-		$res = $dbr->select(
-			[ 'settings' => 'kzchatbot_settings' ],
-			[ 'kzcbs_name', 'kzcbs_value' ],
-			[ 'settings.kzcbs_name' => $generalSettingsNames ],
-			__METHOD__,
-		);
-		$settings = [];
-		for ( $setting = $res->fetchRow(); !empty( $setting ); $setting = $res->fetchRow() ) {
-			$settings[ $setting['kzcbs_name'] ] = $setting['kzcbs_value'];
+		static $settings;
+		if ( !isset( $settings ) ) {
+			$settings = [];
+			$dbr = wfGetDB( DB_REPLICA );
+			$generalSettingsNames = self::getGeneralSettingsNames();
+			$res = $dbr->select(
+				[ 'settings' => 'kzchatbot_settings' ],
+				[ 'kzcbs_name', 'kzcbs_value' ],
+				[ 'settings.kzcbs_name' => $generalSettingsNames ],
+				__METHOD__,
+			);
+			for ( $setting = $res->fetchRow(); !empty( $setting ); $setting = $res->fetchRow() ) {
+				$settings[ $setting['kzcbs_name'] ] = $setting['kzcbs_value'];
+			}
 		}
 		return $settings;
 	}

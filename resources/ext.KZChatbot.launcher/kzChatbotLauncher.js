a2399f53bddd442885c982b840d052bf8212effa
drorsnir <FreedomFighterSparrow@gmail.com>
2024-08-13 12:13:37 +0300

    Use protocol-relative URL for wgServer (rest path)

--- resources/ext.KZChatbot.launcher/kzChatbotLauncher.js
+++ resources/ext.KZChatbot.launcher/kzChatbotLauncher.js
@@ -1,23 +1,24 @@
-mw.loader.using(['mediawiki.cookie'], function () {
+mw.loader.using( [ 'mediawiki.cookie' ], function () {
 
 	// Check cookie
 	const cookieName = 'kzchatbot-uuid';
-	const cookie = mw.cookie.get(cookieName);
-	const uuid = (cookie !== null) ? cookie : '';
+	const cookie = mw.cookie.get( cookieName );
+	const uuid = ( cookie !== null ) ? cookie : '';
 
 	// Build config endpoint
-	const restPath = mw.config.get('wgServer') + mw.config.get('wgScriptPath') + '/rest.php';
+	const serverName = mw.config.get( 'wgServer' ).replace( /^(https?:)?\/\//, '' );
+	const restPath = serverName + mw.config.get( 'wgScriptPath' ) + '/rest.php';
 	const getConfigPath = '/kzchatbot/v0/config';
 	const endpoint = restPath + getConfigPath;
 
 	// Callout to config endpoint
-	$.get(endpoint + '?uuid=' + uuid, function (data) {
+	$.get( endpoint + '?uuid=' + uuid, function ( data ) {
 
 		// (Re-)save cookie
-		mw.cookie.set(cookieName, data.uuid, { expires: new Date(data.cookieExpiry) });
+		mw.cookie.set( cookieName, data.uuid, { expires: new Date( data.cookieExpiry ) } );
 
 		// Is chatbot shown to this user?
-		if (data.chatbotIsShown == "1") {
+		if ( data.chatbotIsShown == "1" ) {
 
 			// Build config data for React app
 			const config = data;
@@ -27,11 +28,11 @@ mw.loader.using(['mediawiki.cookie'], function () {
 			document.body.insertAdjacentHTML('beforeend', '<div id="root"></div>');
 
 			// Launch React app
-			mw.loader.using(['ext.KZChatbot.react'], function () {
+			mw.loader.using( [ 'ext.KZChatbot.react' ], function () {
 
 				// React is loaded and ready.
-
-			});
+				
+			} );
 		}
-	});
-});
+	} );
+} );

14e745ee3c693ae73e7b7ea7b601985c14bb2f79
Aviran Katz <aviran.katz@gmail.com>
2024-08-13 11:05:09 +0300

    feat: Update extension.json with remoteExtPath for KZChatbot resources

--- resources/ext.KZChatbot.launcher/kzChatbotLauncher.js
+++ resources/ext.KZChatbot.launcher/kzChatbotLauncher.js
@@ -1,9 +1,9 @@
-mw.loader.using( [ 'mediawiki.cookie' ], function () {
+mw.loader.using(['mediawiki.cookie'], function () {
 
 	// Check cookie
 	const cookieName = 'kzchatbot-uuid';
-	const cookie = mw.cookie.get( cookieName );
-	const uuid = ( cookie !== null ) ? cookie : '';
+	const cookie = mw.cookie.get(cookieName);
+	const uuid = (cookie !== null) ? cookie : '';
 
 	// Build config endpoint
 	const restPath = mw.config.get('wgServer') + mw.config.get('wgScriptPath') + '/rest.php';
@@ -11,26 +11,27 @@ mw.loader.using( [ 'mediawiki.cookie' ], function () {
 	const endpoint = restPath + getConfigPath;
 
 	// Callout to config endpoint
-	$.get( endpoint + '?uuid=' + uuid, function( data ) {
+	$.get(endpoint + '?uuid=' + uuid, function (data) {
 
 		// (Re-)save cookie
-		mw.cookie.set( cookieName, data.uuid, { expires: new Date( data.cookieExpiry ) } );
+		mw.cookie.set(cookieName, data.uuid, { expires: new Date(data.cookieExpiry) });
 
 		// Is chatbot shown to this user?
-		if ( data.chatbotIsShown == "1" ) {
+		if (data.chatbotIsShown == "1") {
 
 			// Build config data for React app
 			const config = data;
 			config.slugs = mw.config.get('KZChatbotSlugs');
 			config.restPath = restPath;
 			window.KZChatbotConfig = config;
+			document.body.insertAdjacentHTML('beforeend', '<div id="root"></div>');
 
 			// Launch React app
-			mw.loader.using( [ 'ext.KZChatbot.react' ], function() {
+			mw.loader.using(['ext.KZChatbot.react'], function () {
 
 				// React is loaded and ready.
-				
-			} );
+
+			});
 		}
-	} );
-} );
+	});
+});

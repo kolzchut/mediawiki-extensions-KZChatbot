4c5747354f6318dc5c5636237bd099be2b53d726
drorsnir <FreedomFighterSparrow@gmail.com>
2025-01-07 10:40:31 +0200

    Update react code (even larger trigger button on desktop)

--- resources/ext.KZChatbot.launcher/kzChatbotLauncher.js
+++ resources/ext.KZChatbot.launcher/kzChatbotLauncher.js
@@ -1,5 +1,5 @@
 const initializeChatbot = () => {
-	const scriptVersion = 19;
+	const scriptVersion = 20;
 	const cookieName = 'kzchatbot-uuid';
 	const cookie = mw.cookie.get( cookieName );
 	const uuid = cookie !== null ? cookie : '';

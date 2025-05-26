123bbfdb85a4b24a6ead41062adfe9467772f3c8
drorsnir <FreedomFighterSparrow@gmail.com>
2025-05-26 12:58:21 +0300

    Update react code (mobile background overlay)

--- resources/ext.KZChatbot.launcher/kzChatbotLauncher.js
+++ resources/ext.KZChatbot.launcher/kzChatbotLauncher.js
@@ -1,5 +1,5 @@
 const initializeChatbot = () => {
-	const scriptVersion = 23;
+	const scriptVersion = 24;
 	const cookieName = 'kzchatbot-uuid';
 	const cookie = mw.cookie.get( cookieName );
 	const uuid = cookie !== null ? cookie : '';

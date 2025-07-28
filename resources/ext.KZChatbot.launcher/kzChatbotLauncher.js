e92c5cb0bf0fe0424ecd494a755066b902757d0b
drorsnir <FreedomFighterSparrow@gmail.com>
2025-07-28 12:04:51 +0300

    Update react code (bugfixes)

--- resources/ext.KZChatbot.launcher/kzChatbotLauncher.js
+++ resources/ext.KZChatbot.launcher/kzChatbotLauncher.js
@@ -1,5 +1,5 @@
 const initializeChatbot = () => {
-	const scriptVersion = 28;
+	const scriptVersion = 29;
 	const cookieName = 'kzchatbot-uuid';
 	const cookie = mw.cookie.get( cookieName );
 	const uuid = cookie !== null ? cookie : '';

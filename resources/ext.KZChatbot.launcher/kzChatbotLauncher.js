4fd5d4659968e62efc3c67de0a99bc78941b9758
drorsnir <FreedomFighterSparrow@gmail.com>
2026-06-23 01:47:19 +0300

    Update the actual FE code

--- resources/ext.KZChatbot.launcher/kzChatbotLauncher.js
+++ resources/ext.KZChatbot.launcher/kzChatbotLauncher.js
@@ -1,5 +1,5 @@
 const initializeChatbot = () => {
-	const scriptVersion = 35;
+	const scriptVersion = 36;
 	const cookieName = 'kzchatbot-uuid';
 	const cookie = mw.cookie.get( cookieName );
 	const uuid = cookie !== null ? cookie : '';
